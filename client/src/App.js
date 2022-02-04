21c3a65e7eac668c0372251218d5edadb640468b
lpremraj <lpremraj@gmail.com>
2022-02-04 11:45:27 +0530

    corrected typos

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> Fib Calculator verion Kubenetes! </h1>
+          <h1 className="App-title"> Fib Calculator version Kubernetes! </h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Some Other Future Page</Link>
         </header>
